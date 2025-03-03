2cc8651b9a8510b2d6045e37fea53d25cee773c2
spandana <spandana.munagala@gmail.com>
2025-03-03 17:15:40 +0530

    Reapply "commiting files for yq"
    This reverts commit 845694d9a9f18ae39f73eb6c16fc38a9080b1056.

---
+++ test.go
@@ -0,0 +1,12 @@
+package main
+import fmt
+func main() {
+	var a , b = 1, 2
+	if a < b {
+		fmt.Println("a is less than b")
+	} else if a > b {
+		fmt.Println("a is greater than b")
+	} else {
+		fmt. Println(" a is equal to b")
+	}
+}
